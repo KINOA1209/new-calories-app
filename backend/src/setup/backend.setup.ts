ad9f6db09f0ce5a8adb1230b2ea7ee55e0dc80f0
KINOA1209 <108536448+KINOA1209@users.noreply.github.com>
2024-10-10 18:08:48 +0900

    build: setup backend with Express and basic API routes

---
+++ backend/src/setup/backend.setup.ts
@@ -0,0 +1,31 @@
+import express, { Express, Request, Response } from "express";
+import cors from "cors";
+import { json as bodyParser } from "body-parser";
+
+import { Env } from "@/env";
+import { Logger } from "@/utils";
+import { MESSAGE } from "@/constant";
+import { errorHandlerMiddleware, routeMiddleware } from "@/middlewares";
+
+export const backendSetup = () => {
+  const app: Express = express();
+
+  app.use(cors());
+  app.use(bodyParser());
+
+  app.use(routeMiddleware);
+
+  app.use("/health", (_req: Request, res: Response) => {
+    res.send(MESSAGE.SERVER.HELLO_WORLD);
+  });
+
+
+
+  app.use(errorHandlerMiddleware);
+
+  const { port } = Env;
+
+  app.listen(port, () => {
+    Logger.info(MESSAGE.SERVER.STARTING_SUCCESS);
+  });
+};
